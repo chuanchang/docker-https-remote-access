69e55352ad3b859388a77b1e623dd5d524bfadf0
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-05 15:36:25 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -8,10 +8,11 @@ if [ -f /etc/configured ]; then
 else
       #code that need to run only one time ....
       a2ensite default-ssl
+      
       sed -i 's/ProxyPass \/ http:\/\/localhost\//ProxyPass \/ $REMOTE_SITE//' /etc/apache2/conf.d/owncloud.conf
       sed -i 's/ProxyPassReverse \/ http:\/\/localhost\//ProxyPassReverse \/ $REMOTE_SITE//' /etc/apache2/conf.d/owncloud.conf
       
-      #sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
+      sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
         
         #needed for fix problem with ubuntu and cron
         update-locale 
