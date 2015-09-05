8b4277dc862eff5aa09ac42f4a2e586589ae225f
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-05 15:25:37 -0400

    Update pre-conf.sh

--- pre-conf.sh
+++ pre-conf.sh
@@ -10,4 +10,4 @@ rm /etc/apache2/sites-enabled/000-default.conf
 echo "ServerName localhost" | tee /etc/apache2/conf-available/fqdn.conf
 ln -s /etc/apache2/conf-available/fqdn.conf /etc/apache2/conf-enabled/fqdn.conf
 #disable http
-sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
+#sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
