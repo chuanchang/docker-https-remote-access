9b86a71f9c673c5982ade98e386429aa1a79df9a
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-05 15:56:14 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -9,9 +9,11 @@ else
       #code that need to run only one time ....
       a2ensite default-ssl
       
-      sed -i 's/ProxyPass \/ http:\/\/localhost\//ProxyPass \/ $REMOTE_SITE//' /etc/apache2/conf.d/owncloud.conf
-      sed -i 's/ProxyPassReverse \/ http:\/\/localhost\//ProxyPassReverse \/ $REMOTE_SITE//' /etc/apache2/conf.d/owncloud.conf
+      sed -i 's/ProxyPass \/ http:\/\/localhost\//ProxyPass \/ $REMOTE_SITE//' /etc/apache2/sites-available/default-ssl.conf
+      sed -i 's/ProxyPassReverse \/ http:\/\/localhost\//ProxyPassReverse \/ $REMOTE_SITE//' /etc/apache2/sites-available/default-ssl.conf
       
+      #disable http
+      rm /etc/apache2/sites-enabled/000-default.conf
       sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
         
         #needed for fix problem with ubuntu and cron
