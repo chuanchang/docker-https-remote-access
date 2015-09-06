de89bc10a519b87ddcf427f31a5f63ff8534426d
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2015-09-05 21:01:47 -0400

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -5,16 +5,14 @@ set -e
 
 if [ -f /etc/configured ]; then
         echo 'already configured'
+        sed -i "s@ProxyPass / http://localhost/@ProxyPass / $REMOTE_SITE/@" /etc/apache2/sites-available/default-ssl.conf
+        sed -i "s@ProxyPassReverse / http://localhost/@ProxyPassReverse / $REMOTE_SITE/@" /etc/apache2/sites-available/default-ssl.conf
 else
       #code that need to run only one time ....
       a2ensite default-ssl
-      
-      sed -i "s@ProxyPass / http://localhost/@ProxyPass / $REMOTE_SITE/@" /etc/apache2/sites-available/default-ssl.conf
-      sed -i "s@ProxyPassReverse / http://localhost/@ProxyPassReverse / $REMOTE_SITE/@" /etc/apache2/sites-available/default-ssl.conf
-      
       #disable http
-      rm /etc/apache2/sites-enabled/000-default.conf
-      sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
+      #rm /etc/apache2/sites-enabled/000-default.conf
+      #sed -i 's/Listen 80/#Listen 80/' /etc/apache2/ports.conf
         
         #needed for fix problem with ubuntu and cron
         update-locale 
